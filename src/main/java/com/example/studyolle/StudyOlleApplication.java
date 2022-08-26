cdb939ff057c02ada2598a2649aa59a5da869c6a
이경범 <tkdwk567@naver.com>
2022-08-26 23:50:35 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/studyolle/StudyOlleApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOlleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOlleApplication.class, args);
+    }
+
+}
